67e3556e9032caaa240e5463c5fb9b7415f415b1
SentioMe <yjhak1207@naver.com>
2016-09-08 22:09:34 +0900

    pygame set

--- PyGameProjects/160907_0_Begin/GameSources/Main.py
+++ PyGameProjects/160907_0_Begin/GameSources/Main.py
@@ -1,1 +1,22 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
+
+#Required import 'pygame' and 'sys',
+#And pygame in all classes, functions... to import by key(*)
+
+pygame.init()                                           #start pygame lib
+DISPLAYSURF = pygame.display.set_mode((400, 300))       #set pygame display (resolution, flags, depth)
+pygame.display.set_caption('Hello World')               #set display title
+
+while True:                                           #game loop
+    for event in pygame.event.get():                   #check a frame, appeared all events
+        if event.type  == QUIT:                        #If event equals to QUIT(pygame constant value) then, pygame and system end
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()                            #game frame update
+
+
+# it this mean, C++ Console methode
+# pygame is same C++ WinAPI Framework
+# 'print', 'input' is CLI methode
+
